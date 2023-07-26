a38d42a091a9243bef2d88794cd5844205babc05
leogalvan29 <contacto@paginaswebculaican.net>
2023-07-25 20:39:46 -0600

    imagenes arregladas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), image()]
 });
